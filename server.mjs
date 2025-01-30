c36ce01a4f7dcfd251ea3464285e18248a5bb9c9
mvoid <175563124+mvoidets@users.noreply.github.com>
2025-01-30 07:22:31 -0700

    Update server.mjs duplicate removed

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ import { createServer } from 'node:http';
 import next from 'next';
 import { Server } from 'socket.io';
 
-//const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'production';
 const hostname = process.env.HOSTNAME || 'localhost';
 const port = process.env.PORT || '3005';
 
@@ -53,49 +53,3 @@ app.prepare().then(() => {
     console.error('Error preparing Next.js app:', err);
 });
 
-
-const dev = process.env.NODE_ENV !== 'production';
-const hostname = process.env.HOSTNAME || 'localhost';
-const port = process.env.PORT || 3002;
-
-// Set up Redis connection (use Redis URL from Render)
-const redis = new Redis(process.env.REDIS_URL);
-
-// Initialize Next.js app
-const app = next({ dev, hostname, port });
-const handle = app.getRequestHandler();
-
-app.prepare().then(() => {
-  const httpServer = createServer(handle); // Create HTTP server to serve Next.js app
-
-  const io = new Server(httpServer); // Attach Socket.IO to the same server
-  // Use socket.io-redis for pub/sub with Redis
-  //io.adapter(socketIoRedis({ pubClient: redis, subClient: redis }));
-  io.adapter(socketIoRedis({ host: 'localhost', port: 6379 }));
-  
-  io.on('connection', (socket) => {
-    console.log(`a user connected: ${socket.id}`);
-
-    socket.on('join-room', ({ room, username }) => {
-      socket.join(room);
-      console.log(`User ${username} joined room: ${room}`);
-      socket.to(room).emit('user_joined', `${username} joined the room`);
-    });
-
-    socket.on('message', ({ room, message, sender }) => {
-      console.log(`message from ${sender}: in room ${room}: ${message}`);
-      socket.to(room).emit('message', { sender, message });
-    });
-
-    // Handle user disconnections
-    socket.on('disconnect', () => {
-      console.log(`a user disconnected: ${socket.id}`);
-    });
-  });
-
-  // Start the server on the dynamic port
-   httpServer.listen(port, '0.0.0.0', () => {
-        console.log(`Server running on http://${hostname}:${port}`);
-    
-  });
-});
