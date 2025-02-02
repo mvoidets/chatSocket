22988e255900a05d5cd25989564b33204cba1816
mvoid <175563124+mvoidets@users.noreply.github.com>
2025-02-02 13:02:33 -0700

    Update server.mjs pg client

--- server.mjs
+++ server.mjs
@@ -2,7 +2,8 @@
 import { createServer } from 'node:http';
 import next from 'next';
 import { Server } from 'socket.io';
-import { Client } from 'pg'; // Import PostgreSQL client
+import pkg from 'pg';
+const { Client } = pkg;
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = process.env.HOSTNAME || 'localhost';
