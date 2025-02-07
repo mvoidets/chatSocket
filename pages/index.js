4dc9babe799ecd3cebb3bf37a350cce38f6a6ea7
mvoid <175563124+mvoidets@users.noreply.github.com>
2025-02-07 07:40:33 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,3 @@
-export default function Home() { return <h1>Welcome to My App</h1> }
+export default function Home() { 
+     return <h1>Welcome to <a href="https://flex-dice.vercel.app/"> LRC</a></h1> }
+
